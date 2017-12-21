f338b9951fc47096e9330a09ec47bc1bba5248f1
vicfd <vicfd@hotmail.com>
2017-12-21 13:38:19 +0100

    1.1

--- src/test/Visual.java
+++ src/test/Visual.java
@@ -20,16 +20,20 @@ public class Visual extends JFrame
     private void initPanel(double[] hands, int nTest)
     {
         JPanel panel = new JPanel();
-        JLabel[] handText = new JLabel[9];
+        JLabel[] handText = new JLabel[10];
         JProgressBar[] handProgressBar = new JProgressBar[9];        
-        panel.setLayout(new GridLayout(9, 2, 5, 5));
+        panel.setLayout(new GridLayout(10, 2, 5, 5));
         panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         
         for (int i = 0; i < handText.length; ++i) 
         {
             handText[i] = new JLabel();
-            handProgressBar[i] = new JProgressBar();
-            handProgressBar[i].setStringPainted(true);
+            
+            if (i < handProgressBar.length)
+            {
+                handProgressBar[i] = new JProgressBar();
+                handProgressBar[i].setStringPainted(true);
+            }
         }
         
         DecimalFormat df = new DecimalFormat("#.####");
@@ -42,16 +46,16 @@ public class Visual extends JFrame
         handText[6].setText("FULL HOUSE: (" + df.format((hands[6] / nTest) * 100) + "%)");
         handText[7].setText("4 OF A KIND: (" + df.format((hands[7] / nTest) * 100) + "%)");
         handText[8].setText("STRAIGHT FLUSH: (" + df.format((hands[8] / nTest) * 100) + "%)");
+        handText[9].setText("Number of tests: " + nTest);
 
         for (int i = 0; i < handProgressBar.length; ++i)
-            handProgressBar[i].setValue((int) ((hands[i] / nTest) * 100));
-        
-        for (int i = 0; i < handText.length; ++i)
         {
+            handProgressBar[i].setValue((int) ((hands[i] / nTest) * 100));
             panel.add(handText[i]);
-            panel.add(handProgressBar[i]);
+            panel.add(handProgressBar[i]);        
         }
         
+        panel.add(handText[9]);
         this.add(panel);
     }
     
